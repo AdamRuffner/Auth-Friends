b2c4f5b681266f71165885455097554c95bf78cf
Adam <ruffneradam@aol.com>
2020-10-13 20:12:18 -0400

    mvp met, making id link

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,1 +1,55 @@
-// import React, { useState }
+import React, { useState } from "react";
+
+
+const initialValues = {
+  name: "",
+  age: "",
+  email: "",
+};
+
+export default function FriendForm(props) {
+  const [values, setValues] = useState(initialValues);
+
+  const onChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    props.onSubmit(values);
+    setValues(initialValues);
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="name">Name: </label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={values.name}
+        onChange={onChange}
+      />
+      <label htmlFor="age">Age:</label>
+      <input
+        type="text"
+        id="age"
+        name="age"
+        value={values.age}
+        onChange={onChange}
+      />
+      <label htmlFor="email">email:</label>
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={values.email}
+        onChange={onChange}
+      />
+      <button>Make a new friend</button>
+    </form>
+  );
+}
--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import FriendForm from "./FriendForm";
 
 class Friends extends React.Component {
   state = {
@@ -21,9 +22,22 @@ class Friends extends React.Component {
       });
   };
 
+  onSubmit = (data) => {
+    axiosWithAuth()
+      .post("/api/friends", data)
+      .then((res) => {
+        this.setState({
+          ...this.state,
+          friends: res.data,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div className="friends">
+        <FriendForm onSubmit={this.onSubmit} />
         {this.state.friends.map((friend) => (
           <>
             <div className="friend-container">
